6e8ac111c5a4abab7f9c7c3f4aa189d00c1bdb29
Dr. Delyan Malchev <39366200+DrMalchev@users.noreply.github.com>
2021-08-24 23:44:38 +0300

    Final push

---
+++ Csharp_Fundamentals/Excercise_Dictionaries_Extended/Excercise_Dictionaries/01 Anonymos Cache/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Anonymos_Cache
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			Dictionary<string, Dictionary<string, long>> data = new Dictionary<string, Dictionary<string, long>>();
+			Dictionary<string, Dictionary<string, long>> cache = new Dictionary<string, Dictionary<string, long>>();
+			string dataset = "";
+			string datakey = "";
+			long datasize = 0;
+
+			while (input!= "thetinggoesskrra")
+			{
+				string[] info = input.Split(" ->|".ToCharArray(),StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+				if (info.Length==1)
+				{
+					dataset = info[0];
+					if (data.ContainsKey(dataset)==false)
+					{
+						Dictionary<string, long> current = new Dictionary<string, long>();
+						if (cache.ContainsKey(dataset))
+						{
+							current = cache[dataset];
+						}
+						data.Add(dataset,current);
+					}
+				}
+				else
+				{
+					dataset = info[2];
+					datasize = long.Parse(info[1]);
+					datakey = info[0];
+
+					if (data.ContainsKey(dataset)==false)
+					{
+						Dictionary<string, long> current = new Dictionary<string, long>();
+						current.Add(datakey,datasize);
+						if (cache.ContainsKey(dataset)==false)
+						{
+						cache.Add(dataset, current);
+						}
+						else
+						{
+							//todo or do nothing, only his majesty judge will tell
+							cache[dataset].Add(datakey, datasize);
+						}
+					}
+					else
+					{
+						data[dataset].Add(datakey, datasize);
+					}
+
+				}
+
+				input = Console.ReadLine();
+			}
+
+			string print = "";
+			foreach (var pair in data.OrderByDescending(x=>x.Value.Values.Sum()))
+			{
+				Console.WriteLine($"Data Set: {pair.Key}, Total Size: {pair.Value.Values.Sum()}");
+				foreach (var pair2 in pair.Value)
+				{
+					Console.WriteLine($"$.{pair2.Key}");
+
+				}
+				break;
+			}
+
+		}
+	}
+}
---
+++ Csharp_Fundamentals/Excercise_Dictionaries_Extended/Excercise_Dictionaries/02 Hornet Armada/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Hornet_Armada
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int n = int.Parse(Console.ReadLine());
+			Dictionary<string, Dictionary<string, long>> army = new Dictionary<string, Dictionary<string, long>>();
+			Dictionary<string, int> actDict = new Dictionary<string, int>();
+
+			for (int i = 0; i < n; i++)
+			{
+
+				string[] info = Console.ReadLine().Split(" =->:".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+				int activity = int.Parse(info[0]);
+				string name = info[1];
+				string type = info[2];
+				long count = long.Parse(info[3]);
+
+				if (army.ContainsKey(name)==false)
+				{
+					Dictionary<string, long> current = new Dictionary<string, long>();
+					current.Add(type,count);
+					
+					army.Add(name, current);
+
+				}
+				else
+				{
+					if (army[name].ContainsKey(type)==false)
+					{
+						army[name].Add(type, count);
+					}
+					else
+					{
+						army[name][type] += count;
+					}
+					
+				}
+
+				if (actDict.ContainsKey(name)==false)
+				{
+					actDict.Add(name, activity);
+				}
+				else
+				{
+					if (actDict[name]<activity)
+					{
+						actDict[name] = activity;
+					}
+				}
+			}
+
+			string[] commands = Console.ReadLine().Split("\\".ToCharArray(),StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+			if (commands.Length==1)
+			{
+				foreach (var pair in actDict.OrderByDescending(x=>x.Value))
+				{
+					if (army[pair.Key].ContainsKey(commands[0]))
+					{
+					Console.WriteLine($"{pair.Value} : {pair.Key}");
+					}
+
+				}
+			}
+			else
+			{
+				foreach (var pair in army.Where(x=>x.Value.ContainsKey(commands[1])).OrderByDescending(x=>x.Value[commands[1]]))
+				{
+					if (actDict[pair.Key]<int.Parse(commands[0]))
+					{
+						Console.WriteLine($"{pair.Key} -> {pair.Value[commands[1]]}");
+					}
+				}
+			}
+
+		}
+	}
+
+}
